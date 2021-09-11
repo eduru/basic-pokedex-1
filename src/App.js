b7078846c1b641b6f2a461e4e72d0f56fef91054
Eduardo Ruiz <eduru@me.com>
2021-09-12 07:10:05 +0900

    initial setup and poke api calls created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit-20"
+  );
+  const getAllPokemons = async () => {
+    const data = await axios(loadMore);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Pokedex!</h1>
+      <div className="pokemon-container">
+        <div className="all-container"></div>
+        <button className="load-more">Load more</button>
+      </div>
     </div>
   );
 }
